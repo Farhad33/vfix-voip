aa569d06d1758642a48c47cc808e8ee421d57b35
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-09-15 22:07:54 +0330

    Complete responsiv navbar

--- src/common/navbar/Navbar.js
+++ src/common/navbar/Navbar.js
@@ -1,44 +1,47 @@
-import Link from 'next/link';
-import {Container, Button} from '../MainStyle'
-import {NavContainer, NavLinks, Ul, Li,StyledLink} from '../navbar/NavbarStyle'
+'use client'
+import { Data } from '../Data';
+import {Button} from '../MainStyle'
+import {NavContainer, NavLinks, UlList, LiItems, StyledLink, Container, SpanLinks, MenuButton, SpanButton} from '../navbar/NavbarStyle'
 import Image from 'next/image'
-import logovfix from '../photos/vfixlogo.png'
+import { TfiMenu, TfiClose } from "react-icons/tfi";
+import { useState } from 'react';
 
 export default function Navbar() {
 
+    const [isOpen, setIsOpen] = useState(false);
     
+
+    const toggleMenu = () => {
+        console.log('toggleMenu==>', );
+        setIsOpen(!isOpen)
+    }
     return (
         <Container>
             <NavContainer>
                 <Image
-                src={logovfix}
-                alt='Vfix logo'
+                src={Data.navbar.img}
+                alt={Data.navbar.alt}
                 width={140}
+                height={40}
                 />
-                <NavLinks>
-                    <Ul>
-                        <Li>
-                            <StyledLink href='/'>Home</StyledLink>
-                        </Li>
-                        <Li>
-                            <StyledLink href='/whyvfix'>Why Vfix</StyledLink>
-                        </Li>
-                        <Li>
-                            <StyledLink href='/servic'>Services</StyledLink>
-                        </Li>
-                        <Li>
-                            <StyledLink href='/pricing'>Pricing & FAQs</StyledLink>
-                        </Li>
-                        <Li>
-                            <StyledLink href='/company'>Company</StyledLink>
-                        </Li>
-                        <Li>
-                            <StyledLink href='/down'>Downloads</StyledLink>
-                        </Li>
-                        <Li>
-                            <StyledLink href='/login'><Button>Login</Button></StyledLink>
-                        </Li>
-                    </Ul>
+                <MenuButton onClick={toggleMenu}>
+                    {isOpen ? <TfiClose /> : <TfiMenu />}
+                </MenuButton>
+                <NavLinks isOpen = {isOpen}>
+                    <UlList>
+                        {
+                            Data.navbar.menu.map((items, index) =>(
+                                <LiItems key={index}>
+                                    <StyledLink href={Data.navbar.links[index]}>
+                                        <SpanLinks>{items}</SpanLinks>
+                                    </StyledLink>
+                                </LiItems>
+                            ))
+                        }
+                        <LiItems>
+                            <StyledLink href={Data.navbar.login}><Button><SpanButton>Login</SpanButton></Button></StyledLink>
+                        </LiItems>
+                    </UlList>
                 </NavLinks>
             </NavContainer>
         </Container>
--- src/common/navbar/NavbarStyle.js
+++ src/common/navbar/NavbarStyle.js
@@ -1,34 +1,72 @@
 'use client'
+import Link from "next/link";
 import styled from "styled-components";
 
+export const Container = styled.div`
+    width: 100%;
+    padding: 0 5%;
+`
 
 export const NavContainer = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    padding-top: 25px;
 `
 
 export const NavLinks = styled.div`
-    
+    @media (max-width: 768px) {
+    display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+    position: absolute;
+    top: 5%;
+    left: 0;
+    width: 90%;
+    padding: 20px;
+    z-index: 1;
+    background:  linear-gradient(to bottom, #00D59C, #009CD5);
+  }
 `
-export const Ul = styled.ul`
+
+export const MenuButton = styled.button`
+    display: none;
+  @media (max-width: 768px) {
+    display: block;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    font-size: 24px;
+    position: absolute;
+    top: 20px;
+    right: 70px;
+    z-index: 2; 
+    }
+`
+export const UlList = styled.ul`
     display: flex;
     justify-content: space-between;
     align-items: center;
     list-style: none;
+    @media (max-width: 640px) {
+        flex-direction: column;
+    }
 `
 
-export const Li = styled.li`
+export const LiItems = styled.li`
     margin-left: 40px;
     color: #000000;
     text-decoration: none;
     font-size: 16px;
     font-weight: 600;
     letter-spacing:0.15px;
+    @media (max-width: 640px) {
+        padding: 15px 0;
+        font-size: 25px;
+        color: #FFFFFF;
+    }
 `
 
 
-export const StyledLink = styled.a`
+export const StyledLink = styled(Link)`
     color: #000000;
     text-decoration: none;
     &:hover {
@@ -45,4 +83,24 @@ export const StyledLink = styled.a`
     }
 `;
 
-
+export const SpanLinks = styled.span`
+    padding: 0 18px;
+    
+    @media  (max-width: 1200px) {
+        padding:0 5px ;
+    }
+    @media  (max-width: 768px) {
+        padding:0 0 ; 
+    }
+    @media (max-width: 640px) {
+        padding: 5px 0;
+        font-size: 18px;
+        color: #FFFFFF;
+    }
+`
+export const SpanButton = styled.span`
+  @media (max-width: 640px) {
+        font-size: 16px;
+        padding: 0 30px;
+    }
+`
